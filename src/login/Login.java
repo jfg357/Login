0ff4a9e457c0f20e584f7331fed2c21486f032e9
Juan Gaviria <jufgav@gmail.com>
2018-09-01 10:39:12 -0400

    Add comments and change color on event handler message

--- src/login/Login.java
+++ src/login/Login.java
@@ -1,3 +1,10 @@
+/* 2 Creating a Form in JavaFX
+    Creating a form is a common activity when developing an application.
+    This tutorial teaches you the basics of screen layout, how to add controls to a layout pane,
+    and how to create input events.
+    In this tutorial, you will use JavaFX to build the login form
+*/
+
 package login;
 
 import javafx.application.Application;
@@ -17,6 +24,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import jdk.nashorn.internal.ir.*;
 
 public class Login extends Application {
   public static void main(String[] args) {
@@ -24,51 +32,53 @@ public class Login extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage) {
-    primaryStage.setTitle("JavaFX Welcome");
+  public void start(Stage primaryStage) { // This is where the magic happens
 
-    GridPane grid = new GridPane();
-    grid.setAlignment(Pos.CENTER);
-    grid.setHgap(10);
-    grid.setVgap(10);
-    grid.setPadding(new Insets(25, 25, 25, 25));
+    /* Stage settings */
+    primaryStage.setTitle("JavaFX Welcome"); // Set the title
+    GridPane grid = new GridPane(); // Using a GridPane layout because flexibility
+    grid.setAlignment(Pos.CENTER); // Center the GridPane
+    grid.setHgap(10); // Space between columns
+    grid.setVgap(10); // Space between rows
+    grid.setPadding(new Insets(25, 25, 25, 25)); // Padding the context
 
-    Text scenetitle = new Text("Welcome");
-    scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-    grid.add(scenetitle, 0, 0, 2, 1);
+    /* Scene Title settings */
+    Text scenetitle = new Text("Welcome"); // Set scene title
+    scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20)); // Make it pretty
+    grid.add(scenetitle, 0, 0, 2, 1); // Position of title
 
-    Label userName = new Label("User Name:");
-    grid.add(userName, 0, 1);
+    /* Labels and Text fields */
+    Label userName = new Label("User Name:"); // Create and set userName label
+    grid.add(userName, 0, 1); // Position the userName label
 
-    TextField userTextField = new TextField();
-    grid.add(userTextField, 1, 1);
+    TextField userTextField = new TextField(); // Create the userTextField text field
+    grid.add(userTextField, 1, 1); // Position the userTextField text field
 
-    Label pw = new Label("Password:");
-    grid.add(pw, 0, 2);
+    Label pw = new Label("Password:"); // Create and set pw label
+    grid.add(pw, 0, 2); // Position the pw label
 
-    PasswordField pwBox = new PasswordField();
-    grid.add(pwBox, 1, 2);
+    PasswordField pwBox = new PasswordField(); // Create the pwBox password field
+    grid.add(pwBox, 1, 2); // Position the pwBox password field
 
-    Button btn = new Button("Sign in");
+    Button btn = new Button("Sign in"); // Create the button
     HBox hbBtn = new HBox(10);
-    hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-    hbBtn.getChildren().add(btn);
-    grid.add(hbBtn, 1, 4);
+    hbBtn.setAlignment(Pos.BOTTOM_RIGHT); // Position button on the bottom right
+    hbBtn.getChildren().add(btn); // The button is added as a child of the HBox pane
+    grid.add(hbBtn, 1, 4);// HBox pane is added to the grid in column 1, row 4
 
-    final Text actiontarget = new Text();
-    grid.add(actiontarget, 1, 6);
+    final Text actiontarget = new Text(); // Display text
+    grid.add(actiontarget, 1, 6); // Text position
 
-    btn.setOnAction(new EventHandler<ActionEvent>() {
-
+    btn.setOnAction(new EventHandler<ActionEvent>() { // Event handler for sign in button
       @Override
       public void handle(ActionEvent e) {
-        actiontarget.setFill(Color.FIREBRICK);
+        actiontarget.setFill(Color.GREEN);
         actiontarget.setText("Sign in button pressed");
       }
     });
 
     Scene scene = new Scene(grid, 300, 275);
     primaryStage.setScene(scene);
-    primaryStage.show();
+    primaryStage.show(); // Display the scene composed above
   }
 }
